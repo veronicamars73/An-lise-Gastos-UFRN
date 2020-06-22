30b1931970fc02145d9de47511a98231194f8abe
veronicamars73 <mendie73@gmail.com>
2020-06-22 18:13:09 -0300

    Adding Seaborn to the main.py file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+sns.set()
 
 data = pd.read_csv("gastos-por-unidade.csv", sep=";")
 
@@ -10,6 +12,11 @@ imd_process = df.loc[df['unidade']=='INSTITUTO METROPOLE DIGITAL']
 
 ano_sum = imd_process.groupby(["ano"])["valor"].sum()
 
-ano_sum.plot.bar(x="ano")
+ano_sum.plot.bar()
+
+plt.xlabel('Ano')
+plt.ylabel('Valor total de gastos em 10 milhões de reais')
+
+
 
 plt.show()
